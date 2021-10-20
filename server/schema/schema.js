741b6d87aa467076ea1629cd393b56923891b328
codewarrior4 <mayowatope119@gmail.com>
2021-10-20 14:45:03 -0700

    Added author type and nested

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,26 +3,51 @@ const _ = require('lodash')
 
 const {
     GraphQLObjectType,
-    GraphQLString,
     GraphQLSchema,
-    GraphQLID} = graphql
+    GraphQLString,
+    GraphQLID,
+    GraphQLInt
+
+} = graphql
 
 var books =[
-    {name:'Hello world',genre:'World',id:'1'},
-    {name:'Hello wor',genre:'Wor',id:'2'},
-    {name:'Hello worl',genre:'Worl',id:'3'}
+    {name:'Hello world',genre:'World',id:'1',authorid:'1'},
+    {name:'Hello wor',genre:'Wor',id:'2',authorid:'2'},
+    {name:'Hello worl',genre:'Worl',id:'3',authorid:'3'}
 ]
 
+var authors = [
+    {name:"Michael",age:30,id:"1"},
+    {name:"Mayowa",age:13,id:"2"},
+    {name:"Temitope",age:33,id:"3"}
+]
 
 const BookType = new GraphQLObjectType({
     name:'Book',
     fields:()=>({
-        id:{type:GraphQLString},
+        id:{type:GraphQLID},
         name:{type:GraphQLString},
         genre:{type:GraphQLString},
-    })
+        author:{
+            type:AuthorType,
+            resolve(parent){
+                console.log(parent)
+                return _.find(authors,{id:parent.authorid})
+            }
+        }
+    }) 
 })
 
+
+const AuthorType = new GraphQLObjectType({
+    name:'Author',
+    fields:()=>({
+        id:{type:GraphQLID},
+        name:{type:GraphQLString},
+        age:{type:GraphQLInt},
+        
+    })
+})
 
 const RootQuery = new GraphQLObjectType({
     name :'RootQueryType',
@@ -32,6 +57,13 @@ const RootQuery = new GraphQLObjectType({
             args:{id:{type:GraphQLID}},
             resolve(parent,args){
                 return _.find(books,{id:args.id})
+            }
+        },
+        author:{
+            type:AuthorType,
+            args:{id:{type:GraphQLID}},
+            resolve(parent,args){
+                return _.find(authors,{id:args.id})
             }
         }
     }
